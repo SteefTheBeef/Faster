1ac3c877f77c03afff7cdc37126e22469e672eaa
Stefan Hellström <shellstr@gmail.com>
2026-03-04 00:18:39 +0100

    Added umReplays

---
+++ plugins/plugin.41.umReplays.php
@@ -0,0 +1,18 @@
+<?php
+////////////////////////////////////////////////////////////////
+//¤
+// Date:      03.2026
+// Author:    [TnT]BlackCat
+//
+////////////////////////////////////////////////////////////////
+// uncomment the next line to activate the um plugin and see all events in log !
+require_once "helpers/replay/ReplayManager.php";
+
+registerPlugin('umReplays',41);
+
+function umEndRace($event,$Ranking,$ChallengeInfo,$GameInfos,$continuecup,$warmup,$fwarmup){
+	$replayManager = new ReplayManager("/TMF08347/Tracks/Replays/UM");
+	$replayManager->saveBetterReplay($event, $Ranking, $ChallengeInfo, $GameInfos);
+}
+
+?>
