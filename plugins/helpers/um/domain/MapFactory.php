1be8e0e7b7461bb0a6b447329ca05dfda144ae46
Stefan Hellström <shellstr@gmail.com>
2026-03-29 16:07:35 +0200

    Map Factory

---
+++ plugins/helpers/um/domain/MapFactory.php
@@ -0,0 +1,103 @@
+<?php
+
+class MapFactory {
+    public static function um4TA() {
+        return array(
+            // Stadium
+            new UmMap(
+                "rxOudfAAp8TOqTvuPb3ZP4CFFD2",
+                'UM4 TA - BUNKERBUSTER',
+                'shorty_88',
+                'Stadium'
+            ), // Speed
+            new UmMap(
+                "soKTEBzpg_iPyw2A4pPgkF_85J",
+                '$i$fffUM4 TA - $000In aeternum',
+                'jea_quilhas',
+                'Speed'
+            ), // Rally
+            new UmMap(
+                "zYqW_6lKUlVQXqxVOKn6y8QNq_k",
+                '$o$000UM4 TA $0f0Deus $0f0Maximus',
+                'blackcat111',
+                'Rally'
+
+            ), // Bay
+            new UmMap(
+                "nGdljXubw46e83kSOxmMgNUaYg4",
+                'UM4 TA Phantom Island',
+                'kybermato',
+                'Bay'
+            ), // Coast
+            new UmMap(
+                "Qjk_gRmQ2jHMxUhQ6mpo8OETWib",
+                '$o$000UM4 TA $fa3Providence',
+                'blackcat111',
+                'Coast'
+            ), // Alpine
+            new UmMap(
+                "OROjIlKmttKyDLxnvszbkBWp_m0",
+                '$fffUM4 TA United Maniacs',
+                'sandder',
+                'Alpine',
+                'Alpine'
+            ), // Island
+            new UmMap(
+                "ntaH70PYsP6ndT_W5s9oK3tLtC6",
+                '$F00UM4 TA $000Wh$222en $444Lan$666g$888ua$999ge $FFFFails',
+                'mi_me',
+                'Island'
+            ),
+        );
+    }
+
+    public static function um4PO() {
+        return array(
+            // Stadium
+            new UmMap(
+                '8qVWZIvoZqPMXjEMSI3G7dUKGjk',
+                'UM4 PO - BUNKERBUSTER',
+                'shorty_88',
+                'Stadium'
+            ), // Speed
+            new UmMap(
+                'i9IUzbvgNKb7ZWuueIk5EC6Med1',
+                '$i$fffUM4 PO - $000In aeternum',
+                'jea_quilhas',
+                'Speed'
+            ), // Rally
+            new UmMap(
+                'Pt5Noj3YPbmTC_taDdB1QsulCH0',
+                '$o$fffUM4 PO $0f0Deus $0f0Maximus',
+                'blackcat111',
+                'Rally'
+
+            ), // Bay
+            new UmMap(
+                'p0uzz6e0SLND4Z8kpiPomiKuBTk',
+                'UM4 PO Phantom Island',
+                'kybermato',
+                'Bay'
+            ), // Coast
+            new UmMap(
+                'SwLD6NKO9DonFsyMRbcsEBRBnie',
+                '$o$000UM4 PO $fa3Providence',
+                'blackcat111',
+                'Coast'
+            ), // Alpine
+            new UmMap(
+                'lHIylMyu9oR7E7QxIGVBUNr3me2',
+                '$fffUM4 PO United Maniacs',
+                'sandder',
+                'Alpine',
+                'Alpine'
+            ), // Island
+            new UmMap(
+                '_8LbejpxKnMsVgadhNapEdUbqWd',
+                '$F00UM4 PO $000Wh$222en $444Lan$666g$888ua$999ge $FFFFails',
+                'mi_me',
+                'Island'
+            ),
+        );
+    }
+}
